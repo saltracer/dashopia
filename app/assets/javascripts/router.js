799e750d6766cc5ad6ad4b7776149c6a39ea86b6
Paul Swagerty <pswagerty@gmail.com>
2014-07-02 22:16:32 -0700

    Adding tasks to the router

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,5 +1,9 @@
 // For more information see: http://emberjs.com/guides/routing/
 
+App.Router.reopen({
+  location: 'auto'
+});
+
 App.Router.map(function() {
-  // this.resource('posts');
+  this.resource('tasks');
 });
